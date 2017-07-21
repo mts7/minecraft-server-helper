4df030f7eb61503caa17016d4605395b3123e247
Mike <mts7@users.noreply.github.com>
2017-07-20 22:18:05 -0600

    created JavaScript file to give items
    JavaScript file generates the give commands to give specific items with specific enchantments to the nearest player

---
+++ give_tools_armor_weapons.js
@@ -0,0 +1,313 @@
+console.clear();
+
+var enchantments = {
+  aquaAffinity: {
+    item: [
+      'helmet'
+    ],
+    id: 6,
+    lvl: 1
+  },
+  baneOfArthropods: {
+    item: [
+      'sword',
+      'axe'
+    ],
+    id: 18,
+    lvl: 5
+  },
+  blastProtection: {
+    item: [
+      'helmet',
+      'chestplate',
+      'leggings',
+      'boots'
+    ],
+    id: 3,
+    lvl: 4
+  },
+  depthStrider: {
+    item: [
+      'boots'
+    ],
+    id: 8,
+    lvl: 3
+  },
+  efficiency: {
+    item: [
+      'axe',
+      'pickaxe',
+      'shovel',
+      'shears'
+    ],
+    id: 32,
+    lvl: 5
+  },
+  featherFalling: {
+    item: [
+      'boots'
+    ],
+    id: 2,
+    lvl: 4
+  },
+  fireAspect: {
+    item: [
+      'sword'
+    ],
+    id: 20,
+    lvl: 2
+  },
+  fireProtection: {
+    item: [
+      'helmet',
+      'chestplate',
+      'leggings',
+      'boots'
+    ],
+    id: 1,
+    lvl: 4
+  },
+  flame: {
+    item: [
+      'bow'
+    ],
+    id: 50,
+    lvl: 1
+  },
+  fortune: {
+    item: [
+      'axe',
+      'pickaxe',
+      'shovel'
+    ],
+    id: 35,
+    lvl: 3
+  },
+  frostWalker: {
+    item: [
+      'boots'
+    ],
+    id: 9,
+    lvl: 2
+  },
+  infinity: {
+    item: [
+      'bow'
+    ],
+    id: 51,
+    lvl: 1
+  },
+  knockback: {
+    item: [
+      'sword'
+    ],
+    id: 19,
+    lvl: 2
+  },
+  looting: {
+    item: [
+      'sword'
+    ],
+    id: 21,
+    lvl: 3
+  },
+  luckOfTheSea: {
+    item: [
+      'fishing rod'
+    ],
+    id: 61,
+    lvl: 3
+  },
+  lure: {
+    item: [
+      'fishing rod'
+    ],
+    id: 62,
+    lvl: 3
+  },
+  mending: {
+    item: [
+      'shield'
+    ],
+    id: 70,
+    lvl: 1
+  },
+  power: {
+    item: [
+      'bow'
+    ],
+    id: 48,
+    lvl: 5
+  },
+  projectileProtection: {
+    item: [
+      'helmet',
+      'chestplate',
+      'leggings',
+      'boots'
+    ],
+    id: 4,
+    lvl: 4
+  },
+  protection: {
+    item: [
+      'helmet',
+      'chestplate',
+      'leggings',
+      'boots'
+    ],
+    id: 0,
+    lvl: 4
+  },
+  punch: {
+    item: [
+      'bow'
+    ],
+    id: 49,
+    lvl: 2
+  },
+  respiration: {
+    item: [
+      'helmet'
+    ],
+    id: 5,
+    lvl: 3
+  },
+  sharpness: {
+    item: [
+      'sword',
+      'axe'
+    ],
+    id: 16,
+    lvl: 4
+  },
+  silkTouch: {
+    item: [
+      'axe',
+      'pickaxe',
+      'shovel'
+    ],
+    id: 33,
+    lvl: 1
+  },
+  smite: {
+    item: [
+      'sword',
+      'axe'
+    ],
+    id: 17,
+    lvl: 5
+  },
+  sweepingEdge: {
+    item: [
+      'sword'
+    ],
+    id: 22,
+    lvl: 3
+  },
+  thorns: {
+    item: [
+      'helmet',
+      'chestplate',
+      'leggings',
+      'boots'
+    ],
+    id: 7,
+    lvl: 3
+  },
+  unbreaking: {
+    item: [
+      'helmet',
+      'chestplate',
+      'leggings',
+      'boots',
+      'elytra',
+      'sword',
+      'axe',
+      'pickaxe',
+      'shovel',
+      'shears',
+      'bow',
+      'fishing rod',
+      'hoe',
+      'shield'
+    ],
+    id: 34,
+    lvl: 3
+  }
+};
+
+var items = {
+  'sword': {
+    name: 'diamond_sword',
+    qty: 1
+  },
+  'axe': {
+    name: 'diamond_axe',
+    qty: 1
+  },
+  'pickaxe': {
+    name: 'diamond_pickaxe',
+    qty: 1
+  },
+  'shovel': {
+    name: 'diamond_shovel',
+    qty: 1
+  },
+  'bow': {
+    name: 'bow',
+    qty: 1
+  },
+  'hoe': {
+    name: 'diamond_hoe',
+    qty: 1
+  },
+  'helmet': {
+    name: 'diamond_helmet',
+    qty: 1
+  },
+  'chestplate': {
+    name: 'diamond_chestplate',
+    qty: 1
+  },
+  'leggings': {
+    name: 'diamond_leggings',
+    qty: 1
+  },
+  'arrow': {
+    name: 'arrow',
+    qty: 64
+  },
+  'hopper': {
+    name: 'hopper',
+    qty: 64
+  }
+};
+
+
+for (var item in items) {
+  if (!items.hasOwnProperty(item)) {
+    continue;
+  }
+  
+  var cmd = '/give @p minecraft:' + items[item].name + ' ' + items[item].qty;
+  var enchants = [];
+  for (var enchantment in enchantments) {
+    if (!enchantments.hasOwnProperty(enchantment)) {
+      continue;
+    }
+    
+    var enchant = enchantments[enchantment];
+    if (enchant.item.indexOf(item) > -1) {
+      enchants.push('{id:' + enchant.id + ',lvl:' + enchant.lvl + '}');
+    }
+  } // end enchantments
+  
+  if (enchants.length > 0) {
+    cmd += ' {ench[';
+    cmd += enchants.join(',');
+    cmd += ']}';
+  }
+  
+  console.log(cmd);
+}
